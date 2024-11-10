8062d3f45f2a6eeb6e5d332e00ec78a73bdb2773
gladkiy-K <107582240+gladkiy-K@users.noreply.github.com>
2024-11-10 15:28:28 +0300

    Update Main.java

--- src/main/java/guru/qa/gladkiy/Main.java
+++ src/main/java/guru/qa/gladkiy/Main.java
@@ -2,7 +2,11 @@ package guru.qa.gladkiy;
 
 public class Main {
     public static void main(String[] args) {
-
         System.out.println("lets add some different lines");
     }
+
+    private static String(){
+        return "Another one msg!";
+    }
+    
 }
